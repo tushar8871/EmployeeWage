783560fd357e3c90c7891d96013911bbd0e026df
tushar8871 <tusharkhachane6@gmail.com>
2020-03-17 18:20:09 +0530

    Added : Check if employee is present or absent using random

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,4 +2,14 @@
 
 echo "Welcome to employee wage "
 
+present=1;
+attendanceCheck=$((RANDOM%2));
+
+if [ $attendanceCheck -eq $present ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
+
 
