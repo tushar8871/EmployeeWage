748e073fec4f212f778e4f754c2dcb075bd81d77
tushar8871 <tusharkhachane6@gmail.com>
2020-03-18 12:06:47 +0530

    Added : add use case 5 : calculated monthly wage for an employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,24 +3,30 @@
 echo "Welcome to employee wage "
 
 #CONSTANTS
-
 WAGE_PER_HOUR=20;
+DAY_IN_MONTH=20;
 
 #variables
 present=1;
 partTimePresent=2;
-attendanceCheck=$((RANDOM%3));
+empMonthWage=0;
+days=0;
 
-case $attendanceCheck in
-	$present)
-		empHour=8;
-		;;
-	$partTimePresent)
-		empHour=4;
-		;;
-	*)
-		empHour=0;
-		;;
-esac
-
-empDailyWage=$((empHour*WAGE_PER_HOUR));
+while [[ $days -ne 20 ]]
+do
+	attendanceCheck=$((RANDOM%3));
+	case $attendanceCheck in
+		$present)
+			empHour=8;
+			;;
+		$partTimePresent)
+			empHour=4;
+			;;
+		*)
+			empHour=0;
+			;;
+	esac
+	
+	((days++))
+	empMonthWage=$((empHour*WAGE_PER_HOUR+empMonthWage))
+done
