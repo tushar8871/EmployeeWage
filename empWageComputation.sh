a4dfd97c7b3f5bdc175f7b1826390dc475c197d4
tushar8871 <tusharkhachane6@gmail.com>
2020-03-18 10:47:42 +0530

    Added : Add use case 2 : calculated daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,14 +2,23 @@
 
 echo "Welcome to employee wage "
 
+#CONSTANTS
+
+WAGE_PER_HOUR=20;
+FULL_DAY_HOUR=8;
+
+#variables
 present=1;
 attendanceCheck=$((RANDOM%2));
 
 if [ $attendanceCheck -eq $present ]
 then
 	echo "Employee is present"
+	empDailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
+	echo $empDailyWage
 else
 	echo "Employee is absent"
+	empDailyWage=0;
 fi
 
 
