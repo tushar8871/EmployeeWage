e5a8d38f9e968e6f32676400b278885dd61bba22
tushar8871 <tusharkhachane6@gmail.com>
2020-03-18 11:41:13 +0530

    Added : add use case 3 : Add part time present employee and calculate wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,18 +5,20 @@ echo "Welcome to employee wage "
 #CONSTANTS
 
 WAGE_PER_HOUR=20;
-FULL_DAY_HOUR=8;
 
 #variables
 present=1;
-attendanceCheck=$((RANDOM%2));
+partTimePresent=2;
+attendanceCheck=$((RANDOM%3));
 
 if [ $attendanceCheck -eq $present ]
 then
-	echo "Employee is present"
-	empDailyWage=$((WAGE_PER_HOUR*FULL_DAY_HOUR))
-	echo $empDailyWage
+	empHour=8;
+elif [ $attendanceCheck -eq $partTimePresent ]
+then
+	 empHour=4;
 else
-	echo "Employee is absent"
-	empDailyWage=0;
+	empHour=0;
 fi
+
+empDailyWage=$((empHour*WAGE_PER_HOUR));
